5fd4943c646be958ec77575ae74119dca4941be8
Ismail Kassim <jq3583el@go.minnstate.edu>
2021-08-17 21:31:19 -0500

    created google search and feeling lucky buttons

---
+++ script.js
@@ -0,0 +1,34 @@
+let descriptions =["Bored", "Smart", "Busy", "Pizza", "Playful", "Debt", "Noob"];
+let btn = document.getElementById('feeling-lucky');
+
+ // changed text in button when hover over.
+btn.addEventListener('mouseenter', event =>{
+    let selectRandElem = randomNoRepeats(descriptions);
+
+    btn.textContent = `I am Feeling ${selectRandElem()}`;
+    btn.style.fontSize =  '12px';
+ 
+
+});
+
+btn.addEventListener('mouseleave', e => {
+    btn.textContent = "I am Feeling Lucky";
+    btn.style.fontSize =  '12px';
+});
+
+
+ /*
+From stackoverflow: https://stackoverflow.com/a/17891411
+*/
+function randomNoRepeats(array) {
+    var copy = array.slice(0);
+    return function() {
+      if (copy.length < 1) { copy = array.slice(0); }
+      var index = Math.floor(Math.random() * copy.length);
+      var item = copy[index];
+      copy.splice(index, 1);
+      return item;
+    };
+  }
+
+ 
